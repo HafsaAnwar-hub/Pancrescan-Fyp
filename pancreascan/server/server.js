866ee7c13b80f92a8d49ee9b62e4589cca5b4d99
hafsa anwar <nimrraarif@gmail.com>
2026-07-23 15:39:45 +0500

    Configure Vercel serverless deployment

--- pancreascan/server/server.js
+++ pancreascan/server/server.js
@@ -38,4 +38,9 @@ app.get('*', (req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+if (require.main === module) {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
